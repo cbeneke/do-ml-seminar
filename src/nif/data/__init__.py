8d08b164a7a34ad6708da1aeec762d7eb5709a29
Christian Beneke <christian.beneke@traderepublic.com>
2024-12-31 12:13:08 +0100

    Split data out of tensorflow specific implementation (#2)

---
+++ src/nif/data/__init__.py
@@ -0,0 +1,11 @@
+from nif.data.point_wise_data import PointWiseData
+from nif.data.cylinderflow import CylinderFlow
+from nif.data.traveling_wave import TravelingWave
+from nif.data.traveling_wave_high_freq import TravelingWaveHighFreq
+
+__all__ = [
+    "PointWiseData",
+    "CylinderFlow",
+    "TravelingWave",
+    "TravelingWaveHighFreq"
+]
--- src/nif/tf/data/__init__.py
+++
@@ -1,4 +0,0 @@
-from nif.tf.data.point_wise_data import PointWiseData
-from nif.tf.data.tfr_dataset import TFRDataset
-
-__all__ = ["PointWiseData", "TFRDataset"]

5277f2c6cfe41018e55caf066b9570a241a16df4
Christian Beneke <c.beneke@wirelab.org>
2025-01-28 21:20:57 +0100

    Snapshot

--- paper/latex/build.sh
+++ paper/latex/build.sh
@@ -6,4 +6,6 @@ trap 'rm -f main.{aux,bbl,blg,log,out,pdf}' EXIT
 pdflatex main.tex
 bibtex main
 pdflatex main.tex
+pdflatex main.tex # Run twice to resolve references and citations
+
 mv main.pdf ../paper.pdf
